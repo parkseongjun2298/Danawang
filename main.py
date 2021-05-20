f6290311d5c984bb1c477530df78fd9e1c008555
uiwns811 <dltnals5809@gmail.com>
2021-05-20 14:29:25 +0900

    0520 UI 크기 및 좌표 수정

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
 from tkinter import *
 from tkinter import font
 import tkinter.messagebox
-from PIL import ImageTk, Image
 
 g_Tk = Tk()
-g_Tk.geometry("420x500+750+200")
+g_Tk.geometry("500x640+750+200")
 DataList = []
 var1 = IntVar()
 var2 = IntVar()
 
 
 def InitTopText():
-    TempFont = font.Font(g_Tk, size=20, weight='bold', family='Consolas')
+    TempFont = font.Font(g_Tk, size=24, weight='bold', family='Consolas')
     MainText = Label(g_Tk, font=TempFont, text="[Danawang~]")
     MainText.pack()
     MainText.place(x=140)
@@ -19,27 +18,27 @@ def InitTopText():
 
 def InitInputLabel():
     global InputLabel
-    TempFont = font.Font(g_Tk, size=10, weight='bold', family='Consolas')
-    InputLabel = Entry(g_Tk, font=TempFont, width=33, borderwidth=6, relief='ridge')
+    TempFont = font.Font(g_Tk, size=11, weight='bold', family='Consolas')
+    InputLabel = Entry(g_Tk, font=TempFont, width=38, borderwidth=6, relief='ridge')
     InputLabel.pack()
     InputLabel.place(x=120, y=60)
 
 
 def InitSearchButton():
-    TempFont = font.Font(g_Tk, size=10, weight='bold', family='Consolas')
+    TempFont = font.Font(g_Tk, size=12, weight='bold', family='Consolas')
     SearchButton = Button(g_Tk, font=TempFont, text="검색", command=SearchButtonAction)
     SearchButton.pack()
-    SearchButton.place(x=370, y=60)
+    SearchButton.place(x=440, y=60)
 
 
 def MartSearchCheckBox():
-    TempFont = font.Font(g_Tk, size=10, weight='bold', family='Consolas')
+    TempFont = font.Font(g_Tk, size=12, weight='bold', family='Consolas')
     chkbox = Checkbutton(g_Tk, font=TempFont, text='마트 검색', variable=var1, command=SearchMart)
     chkbox2 = Checkbutton(g_Tk, font=TempFont, text='상품 검색', variable=var2)
     chkbox.pack()
     chkbox2.pack()
-    chkbox.place(x=20, y=50)
-    chkbox2.place(x=20, y=70)
+    chkbox.place(x=26, y=60)
+    chkbox2.place(x=26, y=90)
 
 
 def SearchButtonAction():
@@ -83,7 +82,7 @@ def RenderGoodsImage():
     goodsLabel = Label(g_Tk, image=goodsImg)
     goodsLabel.image = goodsImg
     goodsLabel.pack()
-    goodsLabel.place(x=10, y=100)
+    goodsLabel.place(x=17, y=125)
 
 
 def InitButton():
@@ -91,28 +90,28 @@ def InitButton():
     Mapbtn = Button(g_Tk, image=mapImg)
     Mapbtn.image = mapImg
     Mapbtn.pack()
-    Mapbtn.place(x=200, y=425)
+    Mapbtn.place(x=240, y=525)
 
     mailImg = PhotoImage(file="gmail.png").subsample(9, 9)
     Mailbtn = Button(g_Tk, image=mailImg)
     Mailbtn.image = mailImg
     Mailbtn.pack()
-    Mailbtn.place(x=275, y=425)
+    Mailbtn.place(x=320, y=525)
 
     telegramImg = PhotoImage(file="telegram.png").subsample(4, 4)
     Telegrambtn = Button(g_Tk, image=telegramImg)
     Telegrambtn.image = telegramImg
     Telegrambtn.pack()
-    Telegrambtn.place(x=340, y=425)
+    Telegrambtn.place(x=387, y=525)
 
 
 def SearchResultRenderText():
     global RenderText
 
     TempFont = font.Font(g_Tk, size=10, family='Consolas')
-    RenderText = Text(g_Tk, width=38, height=8, borderwidth=6, relief='ridge')
+    RenderText = Text(g_Tk, width=50, height=11, borderwidth=6, relief='ridge')
     RenderText.pack()
-    RenderText.place(x=121, y=90)
+    RenderText.place(x=121, y=95)
 
     RenderText.configure(state='disabled')
 
@@ -125,9 +124,9 @@ def InitRenderText():
     RenderTextScrollbar.place(x=375, y=200)
 
     TempFont = font.Font(g_Tk, size=10, family='Consolas')
-    RenderText = Text(g_Tk, width=25, height=20, borderwidth=6, relief='ridge', yscrollcommand=RenderTextScrollbar.set)
+    RenderText = Text(g_Tk, width=30, height=28, borderwidth=6, relief='ridge', yscrollcommand=RenderTextScrollbar.set)
     RenderText.pack()
-    RenderText.place(x=10, y=215)
+    RenderText.place(x=10, y=252)
     RenderTextScrollbar.config(command=RenderText.yview)
     RenderTextScrollbar.pack(side=RIGHT, fill=BOTH)
 
@@ -137,28 +136,28 @@ def InitRenderText():
 def InitInputMartLabel():
     global InputMartLabel
 
-    TempFont = font.Font(g_Tk, size=10, weight='bold', family='Consolas')
+    TempFont = font.Font(g_Tk, size=12, weight='bold', family='Consolas')
     MainText = Label(g_Tk, font=TempFont, text="판매점 이름을 입력하세요")
     MainText.pack()
-    MainText.place(x=200, y=215)
+    MainText.place(x=245, y=252)
 
     TempFont = font.Font(g_Tk, size=10, weight='bold', family='Consolas')
-    InputMartLabel = Entry(g_Tk, font=TempFont, width=27, borderwidth=6, relief='ridge')
+    InputMartLabel = Entry(g_Tk, font=TempFont, width=31, borderwidth=6, relief='ridge')
     InputMartLabel.pack()
-    InputMartLabel.place(x=200, y=240)
+    InputMartLabel.place(x=245, y=273)
 
 
 def InitSbskButton():
-    TempFont = font.Font(g_Tk, size=15, weight='bold', family='Consolas')
-    selectbtn = Button(g_Tk, font=TempFont, text="판매점 선택")
+    TempFont = font.Font(g_Tk, size=13, weight='bold', family='Consolas')
+    selectbtn = Button(g_Tk, width = 16, font=TempFont, text="판매점 선택")
     selectbtn.pack()
-    selectbtn.place(x=210, y=280)
+    selectbtn.place(x=245, y=305)
 
     sbskImg = PhotoImage(file="장바구니.png").subsample(2)
     sbskbtn = Button(g_Tk, image=sbskImg)
     sbskbtn.image = sbskImg
     sbskbtn.pack()
-    sbskbtn.place(x=340, y=270)
+    sbskbtn.place(x=410, y=305)
 
 
 InitTopText()
